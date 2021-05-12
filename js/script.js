6f5397aac219bd4041bf3068e704b93ac3f21ed9
Hovhannes Petrosyan <Hovopetrosyan7@gmail.com>
2021-05-13 03:47:11 +0400

    js

---
+++ js/script.js
@@ -0,0 +1,98 @@
+// page 35
+
+// // Exercise N1
+
+// let x = +prompt("num x" ,"");
+// let y =0;
+
+// if(x > -10 && x <= -6){
+//   y = x+1;
+// }else if(x<=3){
+//   y = x*x;
+// }else{
+//     y=x;
+// }
+
+// console.log(y);
+
+
+// --------------------------------------------
+
+
+// // Exercise N2
+
+// let a = +prompt("num a" ,"");
+// let b = +prompt("num b" ,"");
+// let x = +prompt("num x" ,"");
+// let y = 0;
+
+// if(a + b > 10){
+//   y= a*x+b;
+// }else if(a+b>= -15 && a+b< 2){
+//   y=b*x-a;
+// }else{
+//     y=a+b;
+// }
+
+// console.log(y);
+
+
+// --------------------------------------------
+
+
+// // Exercise N3
+
+// let x = +prompt ("num x", "");
+// let a = +prompt ("num a", "");
+// let y = 0;
+
+// if(x<=4){
+//     y = x + 3 * a;
+// }else if(x>5 && x<=8){
+//     y = a * x -2;
+// }else if(x>20){
+//    y = x * x ;
+// }else{
+//     y = 3*x;
+// }
+
+// console.log(y);
+
+
+// --------------------------------------------
+
+
+// // Exercise N4
+
+// let n = +prompt ("num n", "");
+
+// if(n % 4 == 0 && n % 400 !== 0){
+//   alert( "nahanj tari e");
+// }else{
+//     alert("nahanj tari che");
+// }
+
+
+// --------------------------------------------
+
+
+
+// // Exercise N5
+
+// let x = +prompt ("num x", "");
+// let y = +prompt ("num y", "");
+// let nx = 0;
+// let ny = 0;
+
+// if(x < y){
+//    nx = (x+y)/2;
+//    ny = (x*y)/2;
+// }else if(x > y){
+//     ny = (x+y)/2;
+//     nx = (x*y)/2;
+// }else{
+//     x = y;
+// }
+
+// alert(nx);
+// alert(ny);
